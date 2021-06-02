009fdeb1b0d42ca6525fd06b23e60825ab19bc1c
Matthew Johnston <mattdjohnston92@gmail.com>
2021-06-02 10:18:45 -0400

    Add js exercise files.

---
+++ js210_small_problems/medium_1/fibonacci_numbers_memoization.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// LS Solution
+
+const memo = {};
+
+function fibonacci(nth) {
+  if (nth <= 2) {
+    return 1;
+  } else {
+    if (memo[nth]) {
+      return memo[nth];
+    } else {
+      memo[nth] = fibonacci(nth - 1) + fibonacci(nth - 2);
+      return memo[nth];
+    }
+  }
+}
